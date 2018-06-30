5d25c2d0d8db7074ac16d91f926826600500dc34
Sarah Jacob <smjacob91@gmail.com>
2018-06-30 15:08:46 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,3 @@
-describe "#introduction" do 
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
-    expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
-  end
-end
 
 
 def introduction
